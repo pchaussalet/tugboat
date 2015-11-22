1e30afb6dc4ea4440c9b285a45f31317f0d84fa3
Peter Souter <peter.souter@puppetlabs.com>
2015-11-22 18:28:44 +0000

    Adding -w flag to ssh
    Using helper method for waiting for active state before SSH-ing.

--- lib/tugboat/middleware/ssh_droplet.rb
+++ lib/tugboat/middleware/ssh_droplet.rb
@@ -50,6 +50,11 @@ module Tugboat
 
         host_string = "#{ssh_user}@#{host_ip}"
 
+        if env['user_droplet_ssh_wait']
+          "Wait flag given, waiting for droplet to become active"
+          wait_for_state(env["droplet_id"],'active',env['barge'])
+        end
+
         say "Attempting SSH: #{host_string}"
 
         options << host_string
